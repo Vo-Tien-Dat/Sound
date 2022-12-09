5c01ddddcc3133a49a341331d303a6d8dc631e90
dat.vt <dat.vt@geekup.vn>
2022-12-09 10:46:14 +0700

    Update constanst file

--- src/main/java/com/music/sound/config/Constant.java
+++ src/main/java/com/music/sound/config/Constant.java
@@ -12,10 +12,6 @@ public interface Constant {
 
     public final String URL_STATIC_AUDIO = "/assets/audio/data/";
 
-    public final String DEFAULT_SOUND_IMAGE = "/assets/img/default/sound_default.png";
-
-    public final String DEFAULT_USER_IMAGE = "/assets/img/default/user_default.png";
-
     // public final String URL_STATIC_AUDIO = "\\audio\\";
 
     public final String URL_STATIC_ADD_PLAYLIST = "playlist\\";
